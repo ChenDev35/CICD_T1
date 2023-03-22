7d5e522f0e98327bef4cbb817067af16746075e4
Chen-Private <chenasulin25@gmail.com>
2023-03-22 13:50:45 +0200

    "added linter"

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import express from 'express';
+
 const port = process.env.port || 3000;
 const app = express();
 
-app.get('/example', (req, res)=>{
-    console.log("new example request arrived");
+app.get('/example', (req, res) => {
+    console.log('new example request arrived');
     res.send('<h1>Hi from class</h1>');
-}
-)
- 
-app.get('/', (req, res)=>{
-    console.log("new request arrived");
+});
+
+app.get('/', (req, res) => {
+    console.log('new request arrived');
     res.send('<h1>Hi from index.js</h1>');
-}
-)
+});
 
-
-
-app.listen(port, ()=>{
-    console.log("server is running at port" + port);
-
-})
+app.listen(port, () => {
+    console.log(`server is running at port: ${port}`);
+});
